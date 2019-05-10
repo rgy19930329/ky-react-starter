34571d98cd57f14b3236db746bce07634935af96
冉光宇 <593289488@qq.com>
2019-05-10 18:03:49 +0800

    rgy

---
+++ app/pages/form/configForm.jsx
@@ -0,0 +1,198 @@
+import React from "react";
+import { InputNumber, Input, DatePicker,  } from "antd";
+import EnumSelect from "@components/EnumSelect";
+import EnumChoice from "@components/EnumChoice";
+import { Form, Button, Row, Col, } from "antd";
+import { fetch } from "@utils";
+import { each, add } from "@utils/wdio";
+
+const ref = new Wilddog("https://kylin.wilddogio.com/test");
+const configRef = ref.child("users");
+
+@Form.create()
+export default class ConfigForm extends React.Component {
+
+  state = {
+    searchList: [
+      {
+        type: "text",
+        field: "name",
+        value: "",
+        label: "姓名",
+        id: "0001",
+      },
+      {
+        type: "number",
+        field: "age",
+        value: "",
+        label: "年龄",
+        id: "0002",
+      },
+      {
+        type: "enumselect",
+        field: "sex",
+        value: "1",
+        label: "性别",
+        id: "0003",
+        spread: {
+          list: [
+            { code: "1", name: "男" },
+            { code: "2", name: "女" }
+          ]
+        }
+      },
+      {
+        type: "text",
+        field: "amount",
+        value: "",
+        label: "金额",
+        id: "0004",
+        spread: {
+          rules: [
+            { required: true, message: "金额不能不空" },
+          ]
+        },
+      },
+      {
+        type: "text",
+        field: "ratio",
+        value: "",
+        label: "百分比",
+        id: "0005",
+      },
+      {
+        type: "text",
+        field: "projectDetail",
+        value: "",
+        label: "项目详情介绍",
+        id: "0006",
+        spread: {
+          placeholder: "请输入项目详情",
+        }
+      },
+    ],
+  }
+
+	componentDidMount() {
+		// this.initFormData();
+		// each(configRef, (item, id) => {
+    //   searchList.push(item);
+    // });
+	}
+
+	/**
+	 * 初始化表单数据
+	 */
+	// initFormData = () => {
+	// 	this.props.form.setFieldsValue({
+			
+	// 	});
+	// };
+
+	/**
+	 * 提交
+	 */
+	submit = (e) => {
+		e.preventDefault();
+		this.props.form.validateFields((err, values) => {
+			if (err) {
+				console.log("Form Values Error!!!");
+				return;
+      }
+      console.log(values);
+		});
+  };
+  
+  /**
+   * 渲染输入控件
+   * @param {Object} 控件属性
+   * @return {JSX}
+   */
+  inputRender = (config) => {
+    const { getFieldDecorator } = this.props.form;
+    const {
+      type,
+      field,
+      value,
+      label,
+      spread = {},
+    } = config;
+    const inputProps = () => {
+      return Object.assign({
+        style: { width: "100%" },
+        ...spread,
+      });
+    }
+    const elementMap = {
+      "number": () => {
+        return (
+          <InputNumber {...inputProps()} />
+        )
+      },
+      "text": () => {
+        return (
+          <Input {...inputProps()} />
+        )
+      },
+      "date": () => {
+        return (
+          <DatePicker {...inputProps()} />
+        )
+      },
+      "enumselect": () => {
+        return (
+          <EnumSelect {...inputProps()} />
+        )
+      }
+    }
+    const formItemLayout = {
+			labelCol: { span: 10 },
+			wrapperCol: { span: 14 }
+		}
+    return (
+      <Form.Item {...formItemLayout} label={label}>
+        {getFieldDecorator(field, {
+          initialValue: value,
+          ...spread,
+        })(
+          elementMap[type] ? elementMap[type]() : `暂不支持${type}组件`
+        )}
+      </Form.Item>
+    )
+  };
+
+	render() {
+		const { searchList } = this.state;
+		return (
+			<div>
+				<h3>配置表单</h3>
+				<Form layout="horizontal" onSubmit={this.submit}>
+          <Row>
+            {searchList && searchList.map((config, index)=> {
+              return (
+                <Col span={8} key={config.id || index}>{this.inputRender(config)}</Col>
+              )
+            })}
+          </Row>
+					<div style={{ textAlign: "right" }}>
+						<Button
+							type="primary"
+							htmlType="submit"
+							icon="search"
+						>
+							查询
+						</Button>
+            <Button
+							type="ghost"
+							htmlType="button"
+              icon="delete"
+              style={{marginLeft: 10}}
+						>
+							重置
+						</Button>
+					</div>
+				</Form>
+			</div>
+		)
+	}
+}
--- app/pages/form/index.jsx
+++ app/pages/form/index.jsx
@@ -2,196 +2,30 @@ import React from "react";
 import { Form, Button, Input } from "antd";
 import EnumSelect from "@components/EnumSelect";
 import EnumChoice from "@components/EnumChoice";
-import { fetch } from "@utils";
-import { each, set, add } from "@utils/wdio";
+import Tabs from "@components/Tabs";
+import OriginForm from "./originForm";
+import ConfigForm from "./configForm";
 
-const ref = new Wilddog("https://kylin.wilddogio.com/test");
-const usersRef = ref.child("users");
-
-@Form.create()
 export default class MyForm extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			searchList: [],
-			searching: false,
-		};
-	}
-
-	componentDidMount() {
-		this.initFormData();
-		each(usersRef, (item, id) => {
-			console.log(id, item);
-		});
-	}
-
-	/**
-	 * 初始化表单数据
-	 */
-	initFormData = () => {
-		this.props.form.setFieldsValue({
-			name: "rgy",
-			age: 25,
-			type: "2",
-			identity: "002",
-			fruit: ["003", "004"],
-		});
-	};
-
-	/**
-	 * 新增数据
-	 */
-	addOne = (item = {}) => {
-		console.log(item);
-		add(usersRef, item);
-	};
-
-	/**
-	 * 提交
-	 */
-	submit = (e) => {
-		e.preventDefault();
-		this.props.form.validateFields((err, values) => {
-			if (err) {
-				console.log("Form Values Error!!!");
-				return;
-			}
-			this.addOne(values);
-		});
-	};
-
-	/**
-	 * 组装goods数据
-	 * @param {Object} data
-	 */
-	fixGoods = (data) => {
-		const list = data.result || [];
-		return list.map(item => {
-			return {
-				code: item[1],
-				name: item[0]
-			}
-		});
-	}
-
 	render() {
-		const { getFieldDecorator } = this.props.form;
-		const formItemLayout = {
-			labelCol: { span: 4 },
-			wrapperCol: { span: 20 }
-		}
 		return (
 			<div>
-				<h2>表单</h2>
-				<Form layout="horizontal" onSubmit={this.submit}>
-					<Form.Item {...formItemLayout} label="Name">
-						{getFieldDecorator("name", {
-							rules: [{
-								required: true,
-								message: "Please input your name",
-							}],
-						})(
-							<Input placeholder="Please input your name" />
-						)}
-					</Form.Item>
-					<Form.Item {...formItemLayout} label="Age">
-						{getFieldDecorator("age", {
-							rules: [{
-								required: true,
-								message: "Please input your age",
-							}],
-						})(
-							<Input placeholder="Please input your age" />
-						)}
-					</Form.Item>
-					<Form.Item {...{
-						labelCol: { span: 4 },
-						wrapperCol: { span: 20 }
-					}} label="Type">
-						{getFieldDecorator("type")(
-							<EnumChoice.Radio list={[
-								{ code: "1", name: "type A" },
-								{ code: "2", name: "type B" },
-							]} />
-						)}
-					</Form.Item>
-					<Form.Item {...{
-						labelCol: { span: 4 },
-						wrapperCol: { span: 4 }
-					}} label="Identity">
-						{getFieldDecorator("identity")(
-							<EnumSelect
-								placeholder="请选择"
-								promiseCondition={this.props.form.getFieldValue("type") || "2"}
-								createPromise={() => fetch({
-									url: "/example/identifyType",
-									data: {
-										type: this.props.form.getFieldValue("type") || "2",
-									}
-								}).then(res => res.data.list || [])}
-							/>
-						)}
-					</Form.Item>
-					<Form.Item {...formItemLayout} label="Fruits">
-						{getFieldDecorator("fruit")(
-							<EnumChoice.Checkbox
-								createPromise={() => fetch({
-									url: "/example/fruits",
-								}).then(res => res.data.list || [])}
-							/>
-						)}
-					</Form.Item>
-					<Form.Item {...{
-						labelCol: { span: 4 },
-						wrapperCol: { span: 12 }
-					}} label="Goods">
-						{getFieldDecorator("goods")(
-							<EnumSelect
-								mode="multiple"
-								showSearch
-								placeholder="请输入您要搜索的商品"
-								labelInValue
-								searchPromise={(value) => fetch({
-									url: "/sug",
-									data: {
-										code: "utf-8",
-										q: value,
-									}
-								}).then(res => {
-									return this.fixGoods(res);
-								})}
-							/>
-						)}
-					</Form.Item>
-					<Form.Item {...{
-						labelCol: { span: 4 },
-						wrapperCol: { span: 12 }
-					}} label="Novels">
-						{getFieldDecorator("novel")(
-							<EnumSelect
-								showSearch
-								placeholder="请输入您要搜索的小说"
-								searchPromise={(value) => fetch({
-									url: "/novelSearchApi",
-									data: {
-										name: value,
-									}
-								}).then(res => {
-									return res.data || [];
-								})}
-							/>
-						)}
-					</Form.Item>
-					<div style={{ textAlign: "right" }}>
-						<Button
-							type="primary"
-							htmlType="submit"
-							icon="check-circle"
-						>
-							提交
-						</Button>
-					</div>
-				</Form>
+				<h2>表单Demo</h2>
+				<Tabs
+					defaultActiveKey="2"
+					panes={[
+						{
+							tab: "原生表单",
+							key: "1",
+							content: <OriginForm />
+						},
+						{
+							tab: "配置表单",
+							key: "2",
+							content: <ConfigForm />
+						}
+					]}
+				/>
 			</div>
 		)
 	}
